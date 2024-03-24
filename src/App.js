52d269867efdb3277fbe17b3c0617dd6f5ee9650
Phantom <abracadabra1253@gmail.com>
2024-03-25 01:28:08 +0400

    yup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React from 'react';
+import {Switch, Route} from 'wouter';
+import Home from './page/Home.js';
+import About from './page/About.js';
+//export default class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+	//render() {
+	
+	
+	
+	export default function App(){
+		
+		
+		  return (<>
+		  
+		  
+		  
+		  <Switch>
+		 
+		 
+		   <Route path="/" component={Home} />
+		   
+		   <Route path="/about" component={About} />
+		   
+		   
+ <Route>
+ Something went off 
+ </Route>
+
+		 
+		 
+		 
+		  </Switch>
+		  
+		  
+		  </>);
+  }
+  
+  
+  
+	//}
+
+
+
+//}
+
+
