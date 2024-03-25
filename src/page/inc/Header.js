b0cdd65351dcae35635305179e11be28b6347a27
Phantom <abracadabra1253@gmail.com>
2024-03-25 19:25:38 +0400

    Header

--- src/page/inc/Header.js
+++ src/page/inc/Header.js
@@ -13,17 +13,28 @@ export function Header(){
 			<div className="row">
 			
 			
-					<div className="col-2">
-						first
+					<div className="col-1">
+							<img style={{    height: '40px'	 }} src="/icons/logo.svg"/>
 						</div>
 				
 				
 						<div className="col-8">
-						center
+								<div className="hdr_centerstyle jcenter">
+										<div> <a href="">Products</a> </div>
+										<div> <a href="">Resources</a> </div>
+										<div> <a href="">Docs</a> </div>
+										<div> <a href="">Prices</a> </div>
+										<div> <a href="">Company</a> </div>
+										<div> <a href="">Contact</a> </div>
+										<div> <a href="">About us</a> </div>
+								</div>
 						</div>
 				
-						<div className="col-2">
-						right
+						<div className="col-3">
+								<div className="fright1">
+									<button className="button">Sign in</button>
+									<button className="button">Sign up</button>
+								</div>
 						</div>
 			
 			
