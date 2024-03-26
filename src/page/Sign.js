fb538401777ecf06b6b15474b969329e1a43dbbc
Phantom <abracadabra1253@gmail.com>
2024-03-26 22:41:37 +0400

    Some changes

--- src/page/Sign.js
+++ src/page/Sign.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
 export function Sign_in(){
-	return (<>sign in</>)
+	return (<>
+	
+	<div class="wrapper_cntr">
+	
+		<div class="h_h1z">Login</div>
+		<div class="textCntq1z">Enter your details</div>
+		
+		<div class="pform12">
+		
+			<input type="text" class="input" placeholder="Username or Email Address"/> 
+		
+			<input type="text" class="input" placeholder="Enter your Password"/>
+		
+
+	
+		
+			<button class="button">Login</button>
+		
+		
+		
+		</div>
+		
+		
+		
+		
+		
+	</div>
+	
+	
+	</>)
 }
 
 
