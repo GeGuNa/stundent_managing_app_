542ed875d5fde305de0b485051d83cd767c43909
Phantom <abracadabra1253@gmail.com>
2024-03-27 00:55:57 +0400

    prices

--- src/page/Prices.js
+++ src/page/Prices.js
@@ -8,14 +8,136 @@ export default function Prices(){
 return (<>
 
 
-Prices <br/>
+<div className="container ttq_qq35q">
 
 
+	<div className="ttq_qq fforHeadings">
+		Plans for teams of any size
+	</div>
 
-<div className="Links">
-	<Link href="/">Home</Link> 
-	<Link href="/about">About</Link>  
-	<Link href="/pricing">Pricing</Link> 
+	<div className="dFle1">
+	
+		<div className="col-4">
+		
+			<div className="wrrpr1ForPriceLists">
+				<h4>
+					Standard
+				</h4>
+				
+			
+				
+				<p className="opacity">
+					Designed for hobbyists who want to start their adventure solo
+				</p>
+				
+					<h3 className="ttq_qq pdng8"> Free for testing </h3>  
+				
+				<p>
+					<button className="button sprice1">Next step</button>
+				</p>
+				
+				<p className="pp">
+					
+					
+					<p>Key features:</p>
+					<ul className="ull_2">
+						<li className="opacity">Monitoring</li>
+						<li className="opacity">Testing and evaluation</li>
+						<li className="opacity">Feature number 1</li>
+						<li className="opacity">Monitoring</li>
+						<li className="opacity">Testing and evaluation</li>
+						<li className="opacity">Feature number 1</li>
+					</ul>
+				
+				</p>
+				
+				
+			</div>
+		
+		</div>
+	
+		<div className="col-4">
+		
+			<div className="wrrpr1ForPriceLists">
+				
+				<h4>
+					Medium
+				</h4>
+				
+				<p className="opacity">
+					Designed for hobbyists who want to start their adventure solo
+				</p>
+				
+				<h3 className="ttq_qq pdng8"> Free for educational purposes </h3>  
+				
+				<p>
+					<button className="button sprice1">Next step</button>
+				</p>
+				
+				
+					<p className="pp">
+					
+					
+					<p>Key features:</p>
+					<ul className="ull_2">
+						<li className="opacity">Monitoring</li>
+						<li className="opacity">Testing and evaluation</li>
+						<li className="opacity">Feature number 1</li>
+						<li className="opacity">Monitoring</li>
+						<li className="opacity">Testing and evaluation</li>
+						<li className="opacity">Feature number 1</li>
+					</ul>
+				
+				</p>
+				
+			</div>
+		
+		</div>
+		
+		
+		<div className="col-4">
+		
+			<div className="wrrpr1ForPriceLists">
+			
+				<h4>
+					Large
+				</h4>
+				
+				<p className="opacity">
+					Designed for hobbyists who want to start their adventure solo
+				</p>
+				
+						<h3 className="ttq_qq pdng8"> Free for the universities </h3>  
+			
+				
+				<p>
+					<button className="button sprice1">Next step</button>
+				</p>
+			
+			
+	
+				<p className="pp">
+					
+					
+					<p>Key features:</p>
+					<ul className="ull_2">
+						<li className="opacity">Monitoring</li>
+						<li className="opacity">Testing and evaluation</li>
+						<li className="opacity">Feature number 1</li>
+						<li className="opacity">Monitoring</li>
+						<li className="opacity">Testing and evaluation</li>
+						<li className="opacity">Feature number 1</li>
+					</ul>
+				
+				</p>
+				
+			</div>
+		
+		</div>
+	
+	
+	
+	</div>
 </div>
 
 
